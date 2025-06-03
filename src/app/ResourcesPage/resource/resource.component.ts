ef6186a6904d3edca30af9960c14abf855a02f8e
Clemzyrex Jnr <etekaclement77@gmail.com>
2025-06-03 22:28:33 +0100

    create the Resource page of the project, needs media query

--- src/app/ResourcesPage/resource/resource.component.ts
+++ src/app/ResourcesPage/resource/resource.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Home/navbar/navbar.component';
 import { ResourceHeroComponent } from "../resource-hero/resource-hero.component";
 import { RemoteCareerComponent } from "../remote-career/remote-career.component";
+import { TogglebtnComponent } from "../../custom/togglebtn/togglebtn.component";
+import { CardComponent } from "../../custom/card/card.component";
+
 @Component({
   selector: 'app-resource',
-  imports: [NavbarComponent, ResourceHeroComponent, RemoteCareerComponent],
+  imports: [NavbarComponent, ResourceHeroComponent, RemoteCareerComponent, TogglebtnComponent, CardComponent],
   templateUrl: './resource.component.html',
   styleUrl: './resource.component.css'
 })
