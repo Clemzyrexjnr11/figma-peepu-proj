39193a6f45ac29381b017db24d996a9451b01bbd
Clemzyrex Jnr <etekaclement77@gmail.com>
2025-06-03 22:26:19 +0100

    create custom toggle button component, still needs modification

---
+++ src/app/custom/togglebtn/togglebtn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-togglebtn',
+  imports: [],
+  templateUrl: './togglebtn.component.html',
+  styleUrl: './togglebtn.component.css'
+})
+export class TogglebtnComponent {
+
+}
